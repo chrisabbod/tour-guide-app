1dc25b2b601f40870debda2123587a3b5f1012c0
Chris <chrisabbod@gmail.com>
2018-06-01 20:50:48 -0400

    <feat>: Create/Populate Location ArrayAdapter in MainActivity
    Now able to display words in list

--- app/src/main/java/com/example/android/tourguideapp/LocationAdapter.java
+++ app/src/main/java/com/example/android/tourguideapp/LocationAdapter.java
@@ -1,7 +1,11 @@
 package com.example.android.tourguideapp;
 
 import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -9,5 +13,22 @@ public class LocationAdapter extends ArrayAdapter<Locations> {
 
     public LocationAdapter(Activity context, ArrayList<Locations> locations){
         super(context, 0, locations);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        View listItemView = convertView;
+
+        if(listItemView == null){
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        Locations currentLocation = getItem(position);
+
+        TextView textView = (TextView)listItemView.findViewById(R.id.title_text_view);
+        textView.setText(currentLocation.getLocationName());
+
+        return listItemView;
     }
 }
--- app/src/main/java/com/example/android/tourguideapp/MainActivity.java
+++ app/src/main/java/com/example/android/tourguideapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.tourguideapp;
 
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ArrayList<Locations> locations = new ArrayList<>();
+        locations.add(new Locations("Fort Wayne"));
+        locations.add(new Locations("Pewabic Pottery"));
+        locations.add(new Locations("GM Renaissance Center"));
+        locations.add(new Locations("Henry Ford Estate"));
+        locations.add(new Locations("Comerica Park"));
+        locations.add(new Locations("Detroit Public Library"));
+        locations.add(new Locations("Greektown"));
+
+        LocationAdapter adapter = new LocationAdapter(MainActivity.this, locations);
+        ListView listView = (ListView)findViewById(R.id.list);
+        listView.setAdapter(adapter);
     }
 }
