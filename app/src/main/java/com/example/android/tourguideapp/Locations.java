9c44a876328c9924f438bbf7dfc88b9c26e80cfa
Chris <chrisabbod@gmail.com>
2018-06-01 20:35:44 -0400

    Create Locations Class

---
+++ app/src/main/java/com/example/android/tourguideapp/Locations.java
@@ -0,0 +1,11 @@
+package com.example.android.tourguideapp;
+
+public class Locations {
+
+    private String mLocationName;
+    private int mImageResource;
+
+    Locations(String locationName){
+        mLocationName = locationName;
+    }
+}
