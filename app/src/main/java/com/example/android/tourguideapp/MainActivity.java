b9ce5c7af203be957a3112ec0a1db29051b847ab
Chris <chrisabbod@gmail.com>
2018-06-02 18:10:38 -0400

    <feat>: Delete ListView Setup from MainActivity

--- app/src/main/java/com/example/android/tourguideapp/MainActivity.java
+++ app/src/main/java/com/example/android/tourguideapp/MainActivity.java
@@ -36,31 +36,7 @@ public class MainActivity extends AppCompatActivity {
                 //close drawer when item is tapped
                 mDrawerLayout.closeDrawers();
 
-
                 return false;
-            }
-        });
-
-        final ArrayList<Locations> locations = new ArrayList<>();
-        locations.add(new Locations("Fort Wayne", R.drawable.fort_wayne));
-        locations.add(new Locations("Pewabic Pottery", R.drawable.pewabic_pottery));
-        locations.add(new Locations("GM Renaissance Center", R.drawable.renaissance_center));
-        locations.add(new Locations("Henry Ford Estate", R.drawable.henry_ford_estate));
-        locations.add(new Locations("Comerica Park", R.drawable.comerica_park));
-        locations.add(new Locations("Detroit Public Library", R.drawable.detroit_public_library));
-        locations.add(new Locations("Greektown", R.drawable.greektown));
-
-        LocationAdapter adapter = new LocationAdapter(MainActivity.this, locations);
-        final ListView listView = (ListView)findViewById(R.id.list);
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
-                intent.putExtra(STRING_TITLE_KEY, locations.get(position).getLocationName());  //Retrieve location title from selected item and pass to detail activity
-                intent.putExtra(IMAGE_RESOURCE_KEY, locations.get(position).getImageResource()); //Pass image resource to detail activity so image can be displayed//Toast.makeText(MainActivity.this, locations.get(position).getImageResource(), Toast.LENGTH_SHORT).show();
-                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/example/android/tourguideapp/PopularPlacesFragment.java
+++ app/src/main/java/com/example/android/tourguideapp/PopularPlacesFragment.java
@@ -24,7 +24,6 @@ public class PopularPlacesFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
