59cc46f23a311bbbaa86ece954925521cccbfa91
Chris <chrisabbod@gmail.com>
2018-06-03 00:53:37 -0400

    <fix>: App No Longer Crashes Due To Large Image Sizes

--- app/src/main/java/com/example/android/tourguideapp/LocationAdapter.java
+++ app/src/main/java/com/example/android/tourguideapp/LocationAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -28,6 +29,9 @@ public class LocationAdapter extends ArrayAdapter<Locations> {
 
         TextView textView = (TextView)listItemView.findViewById(R.id.title_text_view);
         textView.setText(currentLocation.getLocationName());
+
+        ImageView imageView = (ImageView)listItemView.findViewById(R.id.location_image);
+        imageView.setImageResource(currentLocation.getImageResource());
 
         return listItemView;
     }
