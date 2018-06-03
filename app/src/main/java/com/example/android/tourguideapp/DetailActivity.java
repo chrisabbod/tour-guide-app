4b6b88e23ae0888939a9ba82da7677deac6e907b
Chris <chrisabbod@gmail.com>
2018-06-03 18:50:14 -0400

    <chore>: Delete Useless Map Code from DetailActivity

--- app/src/main/java/com/example/android/tourguideapp/DetailActivity.java
+++ app/src/main/java/com/example/android/tourguideapp/DetailActivity.java
@@ -1,20 +1,9 @@
 package com.example.android.tourguideapp;
 
-import android.app.FragmentTransaction;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMapOptions;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-public class DetailActivity extends AppCompatActivity implements OnMapReadyCallback{
+public class DetailActivity extends AppCompatActivity{
     private String stringExtra;
     private int intExtra;
 
@@ -31,21 +20,5 @@ public class DetailActivity extends AppCompatActivity implements OnMapReadyCallb
                 .replace(R.id.detail_fragment_container, new com.example.android.tourguideapp.MapFragment())
                 .commit();
 
-        //initMap();
-    }
-
-    //Initialize and inflate map fragment
-    private void initMap(){
-        MapFragment mapFragment = (MapFragment)getFragmentManager().findFragmentById(R.id.detail_fragment_container);
-        mapFragment.getMapAsync(this);
-    }
-
-    //When map fragment is ready apply marker and move to location
-    @Override
-    public void onMapReady(GoogleMap map){
-        LatLng latLng = new LatLng(42.358750,-82.929737);
-        map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12.0f));
-        map.addMarker(new MarkerOptions().position(latLng).title("TEST"));  //TODO: Make Marker give correct name
-        map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
     }
 }
---
+++ app/src/main/java/com/example/android/tourguideapp/ViewPagerAdapter.java
@@ -0,0 +1,6 @@
+package com.example.android.tourguideapp;
+
+import android.support.v4.app.FragmentActivity;
+
+public class ViewPagerAdapter {
+}
