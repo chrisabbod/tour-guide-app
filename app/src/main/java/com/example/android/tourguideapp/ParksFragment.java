04e9d088b77dd39f7602999d5665d3c7d3be930f
Chris <chrisabbod@gmail.com>
2018-06-04 20:00:59 -0400

    <feat>: Update ParksFragment Locations Constructors

--- app/src/main/java/com/example/android/tourguideapp/ParksFragment.java
+++ app/src/main/java/com/example/android/tourguideapp/ParksFragment.java
@@ -18,7 +18,12 @@ import java.util.ArrayList;
  */
 public class ParksFragment extends Fragment {
     private static final String STRING_TITLE_KEY = "string_title_index";    //Key index used to pass location title to detail activity
+    private static final String STRING_DETAILS_KEY = "string_details_index";
+    private static final String STRING_ADDRESS_KEY = "string_address_index";
+    private static final String STRING_PHONE_KEY = "string_phone_index";
     private static final String IMAGE_RESOURCE_KEY = "image_resource_index";    //Key index used to pass location image resource file
+    private static final String LATITUDE_KEY = "latitude_index";
+    private static final String LONGITUDE_KEY = "longitude_index";
 
     public ParksFragment() {
         // Required empty public constructor
@@ -32,13 +37,20 @@ public class ParksFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.location_list, container, false);
 
         final ArrayList<Locations> locations = new ArrayList<>();
-        locations.add(new Locations(getString(R.string.mariner_park), R.drawable.mariner_park));
-        locations.add(new Locations(getString(R.string.west_riverfront_park), R.drawable.west_riverfront_park));
-        locations.add(new Locations(getString(R.string.grand_circus_park), R.drawable.grand_circus_park));
-        locations.add(new Locations(getString(R.string.belle_isle), R.drawable.belle_isle_park));
-        locations.add(new Locations(getString(R.string.detroit_zoo), R.drawable.detroit_zoo));
-        locations.add(new Locations(getString(R.string.campus_martius_park), R.drawable.campus_martius_park));
-        locations.add(new Locations(getString(R.string.state_park_harbor), R.drawable.william_state_park));
+        locations.add(new Locations(getString(R.string.mariner_park), getString(R.string.mariner_park_details), getString(R.string.mariner_park_address),
+                getString(R.string.mariner_park_number), R.drawable.mariner_park, 42.358750, -82.929737));
+        locations.add(new Locations(getString(R.string.west_riverfront_park), getString(R.string.west_riverfront_park_details), getString(R.string.west_riverfront_park_address),
+                getString(R.string.west_riverfront_park_number), R.drawable.west_riverfront_park, 42.321885, -83.063066));
+        locations.add(new Locations(getString(R.string.grand_circus_park), getString(R.string.grand_circus_park_details), getString(R.string.grand_circus_park_address),
+                getString(R.string.grand_circus_park_number), R.drawable.grand_circus_park, 42.328245, -83.049562));
+        locations.add(new Locations(getString(R.string.belle_isle), getString(R.string.belle_isle_details), getString(R.string.belle_isle_address),
+                getString(R.string.belle_isle_number), R.drawable.belle_isle_park, 42.336514, -82.985275));
+        locations.add(new Locations(getString(R.string.detroit_zoo), getString(R.string.detroit_zoo_details), getString(R.string.detroit_zoo_address),
+                getString(R.string.detroit_zoo_number), R.drawable.detroit_zoo, 42.476489, -83.150798));
+        locations.add(new Locations(getString(R.string.campus_martius_park), getString(R.string.campus_martius_park_details), getString(R.string.campus_martius_park_address),
+                getString(R.string.campus_martius_park_number), R.drawable.campus_martius_park, 42.331563, -83.046835));
+        locations.add(new Locations(getString(R.string.state_park_harbor), getString(R.string.state_park_harbor_details), getString(R.string.state_park_harbor_address),
+                getString(R.string.state_park_harbor_number), R.drawable.william_state_park, 42.333180, -83.025744));
 
         LocationAdapter adapter = new LocationAdapter(getActivity(), locations);
         final ListView listView = (ListView)rootView.findViewById(R.id.list);
@@ -49,7 +61,12 @@ public class ParksFragment extends Fragment {
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                 Intent intent = new Intent(getActivity(), DetailActivity.class);
                 intent.putExtra(STRING_TITLE_KEY, locations.get(position).getLocationName());  //Retrieve location title from selected item and pass to detail activity
+                intent.putExtra(STRING_DETAILS_KEY, locations.get(position).getLocationDetails()); //Retrieve location details
+                intent.putExtra(STRING_ADDRESS_KEY, locations.get(position).getLocationAddress()); //Retrieve location address
+                intent.putExtra(STRING_PHONE_KEY, locations.get(position).getPhoneNumber()); //Retrieve location phone number
                 intent.putExtra(IMAGE_RESOURCE_KEY, locations.get(position).getImageResource()); //Pass image resource to detail activity so image can be displayed//Toast.makeText(MainActivity.this, locations.get(position).getImageResource(), Toast.LENGTH_SHORT).show();
+                intent.putExtra(LATITUDE_KEY, locations.get(position).getLatitude());
+                intent.putExtra(LONGITUDE_KEY, locations.get(position).getLongitude());
                 startActivity(intent);
             }
         });
